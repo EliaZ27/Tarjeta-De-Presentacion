fa19d2668115b2a2c3bc84f6b4718dfe4e25d326
Elias Bravo <Elias Bravo>
2016-10-24 22:58:22 -0600

    first commit

--- app/src/main/java/com/example/usuario/tarjetad_presentacion/MainActivity.java
+++ app/src/main/java/com/example/usuario/tarjetad_presentacion/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.usuario.tarjetad_presentacion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.mas);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, pantalla1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/usuario/tarjetad_presentacion/pantalla1.java
@@ -0,0 +1,11 @@
+package com.example.usuario.tarjetad_presentacion;
+
+import android.os.Bundle;
+
+/**
+ * Created by usuario on 24/10/2016.
+ */
+
+public class pantalla1 {
+
+}
